0caa57d1baaec6cb4f84f0a6f8a84a29a651af78
hassansuhaib <ch.hassan.suhaib@gmail.com>
2021-08-14 01:22:59 +0500

    Added create channel scaffolding

--- frontend/src/containers/VoiceChannels/ChannelList.js
+++ frontend/src/containers/VoiceChannels/ChannelList.js
@@ -3,17 +3,50 @@ import { Link as RouterLink } from 'react-router-dom'
 
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 
-const useStyles = makeStyles((theme) => ({}))
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+}))
 
 const ChannelList = (props) => {
   const classes = useStyles()
   return (
     <div>
-      <h1>ChannelList</h1>
-      <Button variant="contained" component={RouterLink} to="/channels/create">
-        Create Channel
-      </Button>
+      <div className={classes.header}>
+        <h1>ChannelList</h1>
+        <Button
+          variant="contained"
+          component={RouterLink}
+          to="/channels/create"
+        >
+          Create Channel
+        </Button>
+      </div>
+      <div>
+        <List>
+          <ListItem button>
+            Topic: Why Spanish is famous? Language: Espanol Users: 3/4
+          </ListItem>
+          <ListItem button>
+            Topic: Why Spanish is famous? Language: Espanol Users: 3/4
+          </ListItem>
+          <ListItem button>
+            Topic: Why Spanish is famous? Language: Espanol Users: 3/4
+          </ListItem>
+          <ListItem button>
+            Topic: Why Spanish is famous? Language: Espanol Users: 3/4
+          </ListItem>
+          <ListItem button>
+            Topic: Why Spanish is famous? Language: Espanol Users: 3/4
+          </ListItem>
+        </List>
+      </div>
     </div>
   )
 }
